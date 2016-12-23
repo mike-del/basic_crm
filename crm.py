a880ab34ae69407fb8a21401693cce5d84add69e
Michael Delgado <intrepidcreativity@gmail.com>
2016-12-23 14:39:11 -0800

    Added multiple entry for Partner in crm

--- crm.py
+++ crm.py
@@ -20,25 +20,45 @@ def clear():
 		os.system('clear')
 
 def print_options():
-	print('Pick a module: [P]artner or [E]mployee')
-	print('Enter [Q]uit to end this program')
+	print('''
+
+Pick a module: [P]artner or [E]mployee
+(Enter [Q]uit to end this program )
+
+				''')
 	module_choice = input("> ")
 	clear()
 	return module_choice
 
-print('Welcome to Intrepid CRM \n\n')
+clear()
+print('Welcome to Intrepid CRM')
 while True:
 	module_choice = print_options()
 	if module_choice == 'q':
 		break
 	if module_choice in 'pe':
 		if module_choice == 'p':
-			new_record = input('Start new record? ')
+			print('''
+
+Would you like to start a new partner record?
+(Enter [Y]es to create a new partner record
+or enter [N]o to exit. )
+
+				''')
+			new_record = input("> ")
+			clear()
 			if new_record == 'y':
-				new_partner = Partner(Basic_Info())
-				print(new_partner)
+				print('\nHow many new records would you like to enter?\n')
+				num_record = int(input("> "))
+				clear()
+				while num_record:
+					new_partner = Partner(Basic_Info())
+					print(new_partner)
+					num_record -= 1
+					clear()
 		elif module_choice == 'e':
 			new_record = input('Start new record? ')
+			clear()
 			if new_record == 'y':
 				new_emp_id_info = Employee(Basic_Info())
 				print(new_emp_id_info)
--- partner.py
+++ partner.py
@@ -1,5 +1,5 @@
 import random
-
+import basic_info
 from basic_info import Basic_Info
 
 contact_subsection = {}
@@ -8,9 +8,8 @@ partner_memory = {}
 
 
 class Partner(Basic_Info):
-    
     """docstring for Module"""
-    def __init__(self, basic_info, **kwargs):
+    def __init__(self,basic_info, **kwargs):
         def make_partner_id():
             num = random.randint(1,999)
             if num in partner_id:
@@ -18,6 +17,7 @@ class Partner(Basic_Info):
             else:
                 partner_id.append(num)
                 return num
+
         self.partner_uid = make_partner_id()
         self.dept = basic_info.dept
         self.title = basic_info.title
@@ -26,14 +26,9 @@ class Partner(Basic_Info):
         self.phone = basic_info.phone
         self.address = basic_info.address
         self.notes = basic_info.notes
-        # contact_subsection[self.contact] = {'First Name': self.first_name, 'Last Name': self.last_name}
-        # contact_num -= 1
-        # print('{}\n'.format(contact_subsection))
 
         for key, value in kwargs.items():
             setattr(self, key, value)
-
-        
 
         partner_memory[self.partner_uid] = {'Department': self.dept, 'Title': self.title, 'First Name': self.first_name, 'Last Name': self.last_name, 'Phone': self.phone, 'Address': self.address, 'Notes': self.notes, }
 

c51ea198f41fddb84d7f26b16bddfb81d8e9c987
Michael Delgado <intrepidcreativity@gmail.com>
2016-12-22 23:05:40 -0800

    Added Partner Print

--- crm.py
+++ crm.py
@@ -4,8 +4,11 @@ import os
 import sys
 import pdb
 
+import basic_info
 from basic_info import Basic_Info
+import employee
 from employee import Employee
+import partner
 from partner import Partner
 
 logging.basicConfig(filename='newlog.log', level=logging.DEBUG)
@@ -16,21 +19,27 @@ def clear():
 	else:
 		os.system('clear')
 
+def print_options():
+	print('Pick a module: [P]artner or [E]mployee')
+	print('Enter [Q]uit to end this program')
+	module_choice = input("> ")
+	clear()
+	return module_choice
 
 print('Welcome to Intrepid CRM \n\n')
-print('Pick a module: [P]artner or [E]mployee')
-print(datetime.datetime.now())
-module_choice = input("> ")
-clear()
-if module_choice in 'pe':
-	if module_choice == 'p':
-		new_record = input('Start new record? ')
-		if new_record == 'y':
-			new_partner = Partner(Basic_Info())
-			print(new_partner)
-	elif module_choice == 'e':
-		new_record = input('Start new record? ')
-		if new_record == 'y':
-			new_emp_id_info = Employee(Basic_Info())
-			print(new_emp_id_info)
+while True:
+	module_choice = print_options()
+	if module_choice == 'q':
+		break
+	if module_choice in 'pe':
+		if module_choice == 'p':
+			new_record = input('Start new record? ')
+			if new_record == 'y':
+				new_partner = Partner(Basic_Info())
+				print(new_partner)
+		elif module_choice == 'e':
+			new_record = input('Start new record? ')
+			if new_record == 'y':
+				new_emp_id_info = Employee(Basic_Info())
+				print(new_emp_id_info)
 		
